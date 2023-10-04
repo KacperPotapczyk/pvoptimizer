c2d4422cf6a36af0b207d43e4c380123a3e84822
KacperPotapczyk <kacper.potapczyk@gmail.com>
2023-10-04 15:21:06 +0200

    Create kafkaConsumer and docker-compose for environment setup

---
+++ src/main/java/com/github/kacperpotapczyk/pvoptimizer/kafka/kafkaConsumer.java
@@ -0,0 +1,20 @@
+package com.github.kacperpotapczyk.pvoptimizer.kafka;
+
+import com.github.kacperpotapczyk.pvoptimizer.dto.Task;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Component
+public class kafkaConsumer {
+
+    @KafkaListener(topics = "${spring.kafka.consumer.topic}")
+    public void listener(@Payload ConsumerRecord<String, Task> consumerRecord, Acknowledgment acknowledgment) {
+        System.out.println("Received task with id: " + consumerRecord.value().getId());
+        System.out.println("Received task with timeout: " + consumerRecord.value().getTimeoutSeconds());
+        System.out.println("Message: " + consumerRecord.value().toString());
+        acknowledgment.acknowledge();
+    }
+}
